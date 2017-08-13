f65be52dd7858b5d72757af46aa7686312256027
Tim <thung1@binghamton.edu>
2017-08-12 22:53:53 -0400

    Solve access codes, O(n^3) to O(n^2)

--- access-codes/solution.py
+++ access-codes/solution.py
@@ -1,20 +1,21 @@
 """ Tim Hung timhung1010@gmail.com """
 
 def answer(l):
+    """ num_divisors[i] counts the number of
+        divisors of l[i] that come before it """
+    num_divisors = [0] * len(l)
     triple_count = 0
-    for x_i in range(len(l) - 2):
-        for y_i in range(x_i + 1, len(l) - 1):
-            if l[y_i] % l[x_i] == 0:
-                for z_i in range(y_i + 1, len(l)):
-                    if l[z_i] % l[y_i] == 0:
-                        triple_count += 1
+    for large in range(1, len(l)):
+        for small in range (0, large):
+            if l[large] % l[small] == 0:
+                num_divisors[large] += 1
+                triple_count += num_divisors[small]
     return triple_count
 
 def test():
     test_cases = [
         ([1, 1, 1], 1),
         ([1, 2, 3, 4, 5, 6], 3),
-        #([], ),
     ]
     for case in test_cases:
         computed = answer(case[0])
@@ -22,5 +23,6 @@ def test():
             print '\nFailed case!', case[0]
             print 'Computed answer: ', computed
             print 'Expected answer: ', case[-1]
+            return
     print 'All cases passed!'
 test()
