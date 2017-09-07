150edb1f99623233d4dd21ce70677861e3e95039
Tim Hung <thung1@binghamton.edu>
2017-09-06 22:48:59 -0400

    Update expirements to reflect new equations

--- distract-the-guards/banana.py
+++ distract-the-guards/banana.py
@@ -1,3 +1,5 @@
+from fractions import Fraction
+
 def banana_game(b1, b2):
     if b1 == b2:
         return b1, b2
@@ -8,31 +10,33 @@ def banana_game(b1, b2):
 
 def count_banana_games(b1, b2):
     cur = (b1, b2)
-    #print cur
     visited = set([cur])
 
     cur = banana_game(*cur)
 
     rounds_until_stable = 0
     while cur not in visited:
-        #print cur
         rounds_until_stable += 1
         visited.add(cur)
         cur = banana_game(*cur)
-    """
     if cur[0] == cur[1]:
         if b1 < b2:
-            print '{}\t{}\t\t{}'.format(rounds_until_stable, (b1, b2), 1.0 * b2 / b1)
-    """
+            #print '{}\t{}\t\t{}'.format(rounds_until_stable, (b1, b2), 1.0 * b2 / b1)
+            frac = Fraction(b1, b2)
+            print b1, b2, frac.numerator + frac.denominator
     return rounds_until_stable, cur[0] != cur[1]
 
-dim = 512
-for y in range(dim, 0, -1):
-    row = []
-    for x in range (dim):
-        #row.append(' ' if count_banana_games(x, y)[1] else 'X')
-        cbg = count_banana_games(x, y)
-        row.append(' ' if (y < x or cbg[1]) else str(cbg[0]))
-    print y, '\t', ' '.join(row)
-
+def plot_bananas(dim):
+    for y in range(dim, 0, -1):
+        row = []
+        for x in range (dim):
+            row.append(' ' if count_banana_games(x, y)[1] else 'X')
+            cbg = count_banana_games(x, y)
+            row.append(' ' if (y < x or cbg[1]) else str(cbg[0]))
+        print y, '\t', ' '.join(row)
+#plot_bananas(128)
 #print count_banana_games(9, 7)
+
+for y in range(128, 0, -1):
+    for x in range (128):
+        cbg = count_banana_games(x, y)
